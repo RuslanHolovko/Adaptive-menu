44f712cb543b7674515909e0a1beced7fbdf7aa3
tweak15forest <ramagenoisia@gmail.com>
2017-05-20 20:05:43 +0300

    first-commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$('.button').click(function(){
+	$('.nav').toggleClass('showMenu');
+
+	var navig = $('.nav');
+	var showM = $('.showMenu');
+
+	if(navig += showM){
+			$(this).removeClass('.showMenu');
+	}else{
+		return (null);
+	}
+});
